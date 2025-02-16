fb067c9d404b66164a8000d1ea2c60b8564a4eed
marcosgatica2003 <marcosgatica1112@gmail.com>
2025-02-16 14:55:04 -0300

    Implementado de descargar audio del script en python

--- scripts/codigosPython/descargarYT.py
+++ scripts/codigosPython/descargarYT.py
@@ -21,12 +21,12 @@
 
 import os
 import yt_dlp
+import argparse
 
-def download(link):
+def descargarAudioYVideo(link, directorio):
     try:
-        directorioActual = os.getcwd()
         ydl_opts = {
-            'outtmpl': f'{directorioActual}/%(title)s.%(ext)s',
+            'outtmpl': f'{directorio}/%(title)s.%(ext)s',
             'format': 'bestvideo[height<=720]+bestaudio/best[height<=720]',  
             'merge_output_format': 'mp4'
         }
@@ -37,7 +37,41 @@ def download(link):
     except Exception as e:
         print(f"Error: {e}")
 
-if __name__ == "__main__":
-    link = input("Link: ")
-    download(link)
+def descargarAudio(link, directorio):
+    try:
+        ydl_opts = {
+            'outtmpl': f'{directorio}/%(title)s.%(ext)s',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '0',
+            }],
+            
+        }
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([link])
+        print('Hecho')
 
+    except Exception as e:
+        print(f"Error: {e}")
+
+def main():
+    parser = argparse.ArgumentParser(description="Otro script del admin para descargar vídeos y audios de Youtube. Mel no estuvo aquí.")
+    parser.add_argument("--audio", action="store_true", help="Descarga el audio de un vídeo de Youtube.")
+    parser.add_argument("--video", action="store_true", help = "Descarga el audio y vídeo de un vídeo de Youtube en máxima calidad.")
+
+    args = parser.parse_args()
+
+    if not args.audio and not args.video:
+        parser.print_help()
+        return
+
+    directorioActual = os.getcwd()
+    link = input("Link: ")
+    if args.video:
+        descargarAudioYVideo(link, directorioActual)
+    if args.audio:
+        descargarAudio(link, directorioActual)
+
+if __name__ == "__main__":
+    main()
