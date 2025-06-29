5fe28c2f26445d249551551e4b678153be10a692
marcosgatica2003 <marcosgatica1112@gmail.com>
2025-06-29 11:32:04 -0300

    Mejora de toggleTabbed.sh

--- scripts/toggleTabbed.sh
+++ scripts/toggleTabbed.sh
@@ -1,10 +1,35 @@
 #!/bin/bash
 
-winID=$(xdotool getactivewindow)
-winCLASS=$(xprop -id "$winID" WM_CLASS | awk -F '"' '{print $(NF-1)}')
+winId=$(xdotool getactivewindow)
+winClass=$(xprop -id "$winId" WM_CLASS | awk -F '"' '{print $(NF-1)}')
 
-if [ "$winCLASS" = "tabbed" ]; then
-    tabc detach "$winID"
+getTabbedId() {
+    xwininfo -root -tree | grep tabbed | awk '{print $1}' | tail -n1
+}
+
+spawnTabbed() {
+    tabbed -c -n tabbed &
+    sleep 0.3
+}
+
+tabAttach() {
+    local target="$1"
+    local tabbedId=$(getTabbedId)
+    [ -z "$tabbedId" ] && spawnTabbed && tabbedId=$(getTabbedId)
+    xprop -id "$target" -f _TABBED_SELECT_TAB 32c -set _TABBED_SELECT_TAB 0x$(printf %x "$target")
+    xdotool windowreparent "$target" "$tabbedId"
+}
+
+tabDetachAll() {
+    local tabbedId="$1"
+    for child in $(xwininfo -children -id "$tabbedId" | awk '/0x/{print $1}'); do
+        xdotool windowreparent "$child" "$(xwininfo -root | awk '/Window id:/ {print $4}')"
+    done
+}
+
+if [ "$winClass" = "tabbed" ]; then
+    tabDetachAll "$winId"
 else
-    tabc create "$winID"
+    tabAttach "$winId"
 fi
+
