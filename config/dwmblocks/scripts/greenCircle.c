01a9a5165dd18e5dd660a8bfc3c8392ff56eee2b
marcosgatica2003 <marcosgatica1112@gmail.com>
2025-08-18 19:59:53 -0300

    Mecha

---
+++ config/dwmblocks/scripts/greenCircle.c
@@ -0,0 +1,3 @@
+#include <stdio.h>
+
+int main() { printf("^b#282828^^c#3ec07c^ ^d^\n"); return 0; }
---
+++ config/dwmblocks/scripts/redCircle.c
@@ -0,0 +1,3 @@
+#include <stdio.h>
+
+int main() { printf("^b#282828^^c#cc241d^ ^d^\n"); return 0; }
